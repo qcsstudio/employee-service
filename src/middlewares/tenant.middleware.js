567be25b834c6af6a3d80ef1fe12b485e3ce21d8
varunVerse-creater <varunvermadev0@gmail.com>
2026-02-03 18:35:47 +0530

    fixed employee api

--- src/middlewares/tenant.middleware.js
+++ src/middlewares/tenant.middleware.js
@@ -1,31 +1,11 @@
-const axios = require("axios");
-
-module.exports = async (req, res, next) => {
-  try {
-    const tenantHost =
-      req.headers["x-tenant"] ||
-      req.headers.origin ||
-      req.headers.referer;
-
-    if (!tenantHost) {
-      return res.status(400).json({ message: "Tenant header missing" });
-    }
-
-    const hostname = new URL(tenantHost).hostname;
-    const cleanHost = hostname.replace("www.", "");
-
-    const subdomain = cleanHost.split(".")[0];
-
-    const response = await axios.get(
-  `${process.env.AUTH_SERVICE_URL}/auth/companies/resolve/${subdomain}`
-);
-
-
-    req.companyId = response.data.companyId;
-    req.companySlug = subdomain;
-
-    next();
-  } catch (err) {
-    return res.status(404).json({ message: "Invalid tenant" });
+module.exports = (req, res, next) => {
+  // companyId already trusted from auth-service
+  if (!req.user || !req.user.companyId) {
+    return res.status(401).json({ message: "Invalid token" });
   }
+
+  req.companyId = req.user.companyId;
+  req.companySlug = req.user.slug || null; // optional
+
+  next();
 };
