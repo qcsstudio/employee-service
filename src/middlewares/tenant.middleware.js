e2a1bcd7354b75c99766d9ffb8d040fbb554ea60
varunVerse-creater <varunvermadev0@gmail.com>
2026-02-04 09:34:58 +0530

    fixed employee api

--- src/middlewares/tenant.middleware.js
+++ src/middlewares/tenant.middleware.js
@@ -1,11 +1,9 @@
 module.exports = (req, res, next) => {
   try {
-    // JWT already verified by auth middleware
     if (!req.user || !req.user.companyId) {
       return res.status(401).json({ message: "Invalid token" });
     }
 
-    // Tenant header is mandatory for company routes
     const tenantHost = req.headers["x-tenant"];
     if (!tenantHost) {
       return res.status(400).json({ message: "x-tenant header required" });
